d07d51aefe56e4cdee08c2e236b9dd58b3199655
ShivaniAnandan <shivanianandhan28@gmail.com>
2024-10-17 11:35:24 +0530

    updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import logo from '../assets/Askmeidentity 2.png';
-import icons from '../assets/Group 1.png';
+// import icons from '../assets/Group 1.png';
 import iconcart from  '../assets/iconcart.png';
 import { useNavigate, Link } from 'react-router-dom';
 import { myContext } from '../App';
